eece4474fc0f4b6cac7bf1a466203d5552cdc61e
Adriantxu <adrian.callejo-calvo@eptiech.eu>
2023-12-18 13:59:42 +0100

    REvert

--- app/app.ts
+++ app/app.ts
@@ -4,27 +4,7 @@ var app = express();
 
 // Routes
 app.get('/', function(req, res) {
-  // res.send('Hello World!');
-    // Assuming your PNG image is in the same directory as your Node.js script
-    const imagePath = path.join(__dirname, 'app', 'Theo.png');
-
-    // Set the content type to image/png
-    res.contentType('image/png');
-
-    // Send the file as the response
-    res.sendFile(imagePath);
-});
-
-app.get('/theo', function(req, res) {
-  // Assuming your PNG image is in the same directory as your Node.js script
-  const imagePath = path.join(__dirname, 'app', 'Theo.png');
-
-  // Set the content type to image/png
-  res.contentType('image/png');
-
-  // Send the file as the response
-  res.sendFile(imagePath);
-
+  res.send('Hello World!');
 });
 
 // Listen
